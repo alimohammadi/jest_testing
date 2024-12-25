075076f97e24f0d83deeab15545c86cd8f385aa6
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-12-25 15:38:28 +0330

    Improve/ add new db part & write test for fetching error

--- react-testing/tests/components/ProductDetail.test.tsx
+++ react-testing/tests/components/ProductDetail.test.tsx
@@ -1,28 +1,49 @@
 import { render, screen } from "@testing-library/react";
 import ProductDetail from "../../src/components/ProductDetail";
-import { products } from "../mocks/data";
 import { server } from "../mocks/server";
 import { http, HttpResponse } from "msw";
+import { db } from "../mocks/db";
 
 describe("ProductDetail", () => {
-  it("should render the list of products", async () => {
-    render(<ProductDetail productId={1} />);
+  let productId: number;
+
+  beforeAll(() =>
+    [1, 2, 3].forEach(() => {
+      const product = db.product.create();
+      productId = product.id;
+    })
+  );
+
+  afterAll(() => db.product.delete({ where: { id: { equals: productId } } }));
+
+  it("should render product details", async () => {
+    render(<ProductDetail productId={productId} />);
+
+    const product = db.product.findFirst({
+      where: { id: { equals: productId } },
+    });
 
     expect(
-      await screen.findByText(new RegExp(products[0].name))
+      await screen.findByText(
+        new RegExp(
+          db.product
+            .findMany({ where: { id: { equals: productId } } })
+            .toString()
+        )
+      )
     ).toBeInTheDocument();
 
     expect(
-      await screen.findByText(new RegExp(products[0].price.toString()))
+      await screen.findByText(new RegExp(product!.price.toString()))
+    ).toBeInTheDocument();
+
+    expect(
+      await screen.findByText(new RegExp(product!.name.toString()))
     ).toBeInTheDocument();
   });
 
   it("should render message if product not found", async () => {
-    server.use(
-      http.get("/products/:id", () => {
-        return HttpResponse.json(null);
-      })
-    );
+    server.use(http.get("/products/:id", () => HttpResponse.json(null)));
 
     render(<ProductDetail productId={1} />);
 
@@ -34,5 +55,13 @@ describe("ProductDetail", () => {
     render(<ProductDetail productId={0} />);
 
     expect(await screen.findByText(/invalid/i)).toBeInTheDocument();
+  });
+
+  it("should render an error if data fetching fails", async () => {
+    server.use(http.get("/products/:id", () => HttpResponse.error()));
+
+    render(<ProductDetail productId={1} />);
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
   });
 });

a70ed41c818c904385d5aee24de7ef9b1f392d6b
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-12-25 15:38:07 +0330

    Improve/ add new db part & write test for occuring error

--- react-testing/tests/components/ProductList.test.tsx
+++ react-testing/tests/components/ProductList.test.tsx
@@ -2,8 +2,20 @@ import { render, screen } from "@testing-library/react";
 import ProductList from "../../src/components/ProductList";
 import { server } from "../mocks/server";
 import { http, HttpResponse } from "msw";
+import { db } from "../mocks/db";
 
 describe("ProductList", () => {
+  const productIds: number[] = [];
+
+  beforeAll(() =>
+    [1, 2, 3].forEach(() => {
+      const product = db.product.create();
+      productIds.push(product.id);
+    })
+  );
+
+  afterAll(() => db.product.deleteMany({ where: { id: { in: productIds } } }));
+
   it("should render list of products", async () => {
     render(<ProductList />);
 
@@ -18,5 +30,12 @@ describe("ProductList", () => {
 
     const message = await screen.findByText(/no products available./i);
     expect(message).toBeInTheDocument();
+  });
+
+  it("should render error message when there is and error", async () => {
+    server.use(http.get("/products", () => HttpResponse.error()));
+    render(<ProductList />);
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
   });
 });

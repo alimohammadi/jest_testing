abf2aa44d640ef01228391255afa60a6d799b651
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-12-25 15:40:41 +0330

    Improve/ replace old handlers with new ones

--- react-testing/tests/mocks/handlers.ts
+++ react-testing/tests/mocks/handlers.ts
@@ -1,18 +1,3 @@
-import { http, HttpResponse } from "msw";
-import { categories, products } from "./data";
+import { db } from "./db";
 
-export const handlers = [
-  http.get("/categories", () => HttpResponse.json(categories)),
-
-  http.get("/products", () => HttpResponse.json(products)),
-
-  http.get("/products/:id", ({ params }) => {
-    const id = parseInt(params.id as string);
-
-    const product = products.find((product) => product.id === id);
-
-    if (!product) return new HttpResponse(null, { status: 404 });
-
-    return HttpResponse.json(product);
-  }),
-];
+export const handlers = [...db.product.toHandlers("rest")];
